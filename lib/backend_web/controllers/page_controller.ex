e123d8f96a00225d387b6c5cc5ad348717aa15fb
Gustavo Oliveira <g.92oliveira@gmail.com>
2022-02-01 19:55:17 -0300

    show proccess id

--- lib/backend_web/controllers/page_controller.ex
+++ lib/backend_web/controllers/page_controller.ex
@@ -2,7 +2,7 @@ defmodule BackendWeb.PageController do
   use BackendWeb, :controller
 
   def index(conn, _params) do
-    pid = self |> :erlang.pid_to_list |> to_string
+    pid = self |> :erlang.pid_to_list() |> to_string
     render(conn, "index.html", process_id: pid)
   end
 end
